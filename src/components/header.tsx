c27d2e6d124ca3d7dc804d1b5c5d0c9a7a46709a
Michal Stasiak <michal.stasiak@protonmail.com>
2025-09-28 18:04:36 +0200

    add Links to header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -9,14 +9,13 @@ import { useSession } from "next-auth/react";
 import { signOut } from "next-auth/react";
 
 const Header = () => {
-  const { data: session, status } = useSession();
+  const { data: session } = useSession();
   return (
     <div>
       <div className="border-b-[1px] border-b-[var(--gray-200)] mt-8 mx-10 pb-10 ">
         <div className="flex justify-between ">
           <Logo />
           <div className="flex justify-center items-center gap-7">
-            {/* TODO dodac logike odpowiadajaca za zmiane ikonki koszyka i profilu jezeli uzytkownik jest zalogowany */}
             {!!session ? (
               <>
                 <Link href="/cart" className="text-[var(--neutral-900)]">
@@ -48,12 +47,19 @@ const Header = () => {
           </div>
         </div>
         <div className="flex gap-8">
-          <span className="font-semibold text-base text-[var(--primary-500)]">
-            Home
-          </span>
-          <span className="font-medium text-base text-[var(--neutral-500)]">
-            Product
-          </span>
+          <Link href="/">
+            {" "}
+            <span className="font-semibold text-base text-[var(--primary-500)]">
+              Home
+            </span>{" "}
+          </Link>
+
+          <Link href="/product">
+            {" "}
+            <span className="font-medium text-base text-[var(--neutral-500)]">
+              Product
+            </span>
+          </Link>
           <span className="font-medium text-base text-[var(--neutral-500)]">
             Contact
           </span>
