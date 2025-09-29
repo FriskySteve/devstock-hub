3bbad0bd4f7e552977f09879ce67f4c40c44d6ed
Michal Stasiak <michal.stasiak@protonmail.com>
2025-09-29 16:42:21 +0200

    fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -23,6 +23,7 @@ const eslintConfig = [
       "@typescript-eslint/no-require-imports": "off",
       "@typescript-eslint/no-this-alias": "off",
       "@typescript-eslint/no-explicit-any": "off",
+      "@typescript-eslint/*": "off",
     },
   },
 ];
