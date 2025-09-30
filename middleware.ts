0042329f9118623610b75ab77a79885ec2f3d86d
Michal Stasiak <michal.stasiak@protonmail.com>
2025-09-30 18:52:26 +0200

    turn on middleware with more authRoutes

--- middleware.ts
+++ middleware.ts
@@ -1,59 +1,65 @@
-// import { withAuth } from "next-auth/middleware";
-// import { NextResponse } from "next/server";
-// import prisma from "./lib/prisma";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+import prisma from "./lib/prisma";
 
-// const authRoutes = ["/login", "/register", "/register-success"];
-// const publicRoutes = ["/contact"];
+const authRoutes = [
+  "/login",
+  "/register",
+  "/register-success",
+  "/",
+  "/product",
+];
+const publicRoutes = ["/contact"];
 
-// export default withAuth(
-//   async function middleware(req) {
-//     const { pathname } = req.nextUrl;
-//     const token = req.nextauth.token;
+export default withAuth(
+  async function middleware(req) {
+    const { pathname } = req.nextUrl;
+    const token = req.nextauth.token;
 
-//     if (!token?.email) {
-//       return;
-//     }
+    if (!token?.email) {
+      return;
+    }
 
-//     if (token?.email) {
-//       try {
-//         const user = await prisma.user.findUnique({
-//           where: { email: token.email },
-//         });
+    if (token?.email) {
+      try {
+        const user = await prisma.user.findUnique({
+          where: { email: token.email },
+        });
 
-//         if (!user) {
-//           const response = NextResponse.redirect(new URL("/login", req.url));
-//           response.cookies.delete("next-auth.session-token");
-//           response.cookies.delete("next-auth.csrf-token");
-//           return response;
-//         }
-//       } catch (error) {
-//         console.error("Error checking user existence:", error);
-//       }
-//     }
+        if (!user) {
+          const response = NextResponse.redirect(new URL("/login", req.url));
+          response.cookies.delete("next-auth.session-token");
+          response.cookies.delete("next-auth.csrf-token");
+          return response;
+        }
+      } catch (error) {
+        console.error("Error checking user existence:", error);
+      }
+    }
 
-//     if (token && authRoutes.some((route) => pathname.startsWith(route))) {
-//       return NextResponse.redirect(new URL("/", req.url));
-//     }
-//   },
-//   {
-//     callbacks: {
-//       authorized: ({ token, req }) => {
-//         const { pathname } = req.nextUrl;
+    if (token && authRoutes.some((route) => pathname.startsWith(route))) {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token, req }) => {
+        const { pathname } = req.nextUrl;
 
-//         if (
-//           !token &&
-//           (authRoutes.some((route) => pathname.startsWith(route)) ||
-//             publicRoutes.some((route) => pathname.startsWith(route)))
-//         ) {
-//           return true;
-//         }
+        if (
+          !token &&
+          (authRoutes.some((route) => pathname.startsWith(route)) ||
+            publicRoutes.some((route) => pathname.startsWith(route)))
+        ) {
+          return true;
+        }
 
-//         return !!token;
-//       },
-//     },
-//   }
-// );
+        return !!token;
+      },
+    },
+  }
+);
 
-// export const config = {
-//   matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
-// };
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
