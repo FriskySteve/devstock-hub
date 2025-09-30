9d4a1e8c4e8e28a305d1805c06cf59a9290ce148
Michal Stasiak <michal.stasiak@protonmail.com>
2025-09-30 18:49:28 +0200

    turn off middleware

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -39,8 +39,6 @@ export async function POST(req: NextRequest) {
         {
           success: false,
           message: `Invalid credentials, password: ${password}, user.password: ${user.password}`,
-          password: password,
-          userPassword: user.password,
         },
         { status: 401 }
       );
