041f8c7b163986aab16d3e9502bbe8742cef618d
Michal Stasiak <michal.stasiak@protonmail.com>
2025-09-30 16:15:57 +0200

    test2

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,4 @@
-// import { PrismaClient } from "@/app/generated/prisma/client";
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/app/generated/prisma/client";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient;
@@ -8,31 +7,5 @@ const globalForPrisma = global as unknown as {
 const prisma = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-export default prisma;
-
-// import { PrismaClient } from "@prisma/client";
-
-// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
-
-// export const prisma = globalForPrisma.prisma || new PrismaClient();
-
-// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
-
-import { PrismaClient } from "@prisma/client";
-
-declare global {
-  var prisma: PrismaClient | undefined;
-}
-
-let prisma: PrismaClient;
-
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.prisma) {
-    global.prisma = new PrismaClient();
-  }
-  prisma = global.prisma;
-}
 
 export default prisma;
